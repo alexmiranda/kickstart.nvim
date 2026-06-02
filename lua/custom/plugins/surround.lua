d5e8b036e51c0580dc8e7ccdf97f58b494cf21b7
Alex Miranda <alexmirandamoraes@gmail.com>
2026-06-02 13:36:32 +0200

    Convert remaining custom plugins from lazy.nvim to vim.pack

--- lua/custom/plugins/surround.lua
+++ lua/custom/plugins/surround.lua
@@ -1,23 +1,15 @@
 -- Remap mini.surround to tpope-style keybindings
--- Returns empty spec; configuration is applied via autocmd after mini.nvim loads
+-- mini.nvim is loaded in init.lua; this reconfigures surround mappings
 
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'VeryLazy',
-  once = true,
-  callback = function()
-    require('mini.surround').setup({
-      mappings = {
-        add = 'ys',
-        delete = 'ds',
-        replace = 'cs',
-        find = '',
-        find_left = '',
-        highlight = '',
-        update_n_lines = '',
-      },
-    })
-    vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { desc = 'Add surrounding' })
-  end,
-})
-
-return {}
+require('mini.surround').setup {
+  mappings = {
+    add = 'ys',
+    delete = 'ds',
+    replace = 'cs',
+    find = '',
+    find_left = '',
+    highlight = '',
+    update_n_lines = '',
+  },
+}
+vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { desc = 'Add surrounding' })
--- lua/custom/plugins/zen.lua
+++ lua/custom/plugins/zen.lua
@@ -1,17 +1,15 @@
 -- Zen mode for focused editing
 
----@module 'lazy'
----@type LazySpec
-return {
-  {
-    'folke/zen-mode.nvim',
-    keys = {
-      { '<leader>tz', '<cmd>ZenMode<cr>', desc = '[T]oggle [Z]en mode' },
-    },
-    opts = {
-      window = {
-        width = 120,
-      },
-    },
+---@param repo string
+---@return string
+local function gh(repo) return 'https://github.com/' .. repo end
+
+vim.pack.add { gh 'folke/zen-mode.nvim' }
+
+require('zen-mode').setup {
+  window = {
+    width = 120,
   },
 }
+
+vim.keymap.set('n', '<leader>tz', '<cmd>ZenMode<cr>', { desc = '[T]oggle [Z]en mode' })
